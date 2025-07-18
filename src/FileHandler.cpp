89425778fc713a85a5079df505acdc2991deabbe
rcallaby <rcallaby@gmail.com>
2025-07-18 08:45:00 -0500

    added a filehandler functionality to the repo

---
+++ src/FileHandler.cpp
@@ -0,0 +1,25 @@
+#include "FileHandler.h"
+#include <fstream>
+#include <sstream>
+
+std::string FileHandler::readHashFromFile(const std::string& filePath) {
+    std::ifstream file(filePath);
+    if (!file.is_open()) return "";
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+}
+
+rapidjson::Document FileHandler::loadConfigFromFile(const std::string& configFilePath) {
+    rapidjson::Document doc;
+    FILE* fp = fopen(configFilePath.c_str(), "rb");
+    if (!fp) return doc;
+    char readBuffer[65536];
+    rapidjson::FileReadStream is(fp, readBuffer, sizeof(readBuffer));
+    doc.ParseStream(is);
+    fclose(fp);
+    return doc;
+}
+
+You should create FileHandler.cpp and add these implementations.
+
