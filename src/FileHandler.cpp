319c7b7fd4df6d07d693a9e00eea07932a9c507f
rcallaby <rcallaby@gmail.com>
2025-08-17 18:46:13 -0500

    hopefully debugged the program

--- src/FileHandler.cpp
+++ src/FileHandler.cpp
@@ -1,24 +1,42 @@
 #include "FileHandler.h"
 #include <fstream>
 #include <sstream>
+#include <stdexcept>
+#include <rapidjson/error/en.h>
 
 std::string FileHandler::readHashFromFile(const std::string& filePath) {
     std::ifstream file(filePath);
-    if (!file.is_open()) return "";
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    return buffer.str();
+    if (!file.is_open()) {
+        throw std::runtime_error("Unable to open hash file: " + filePath);
+    }
+    std::string hash;
+    file >> hash;
+    return hash;
 }
 
-rapidjson::Document FileHandler::loadConfigFromFile(const std::string& configFilePath) {
+rapidjson::Document FileHandler::loadConfigFromFile(const std::string& filePath) {
+    std::ifstream file(filePath);
+    if (!file.is_open()) {
+        throw std::runtime_error("Unable to open config file: " + filePath);
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    std::string jsonStr = buffer.str();
+
     rapidjson::Document doc;
-    FILE* fp = fopen(configFilePath.c_str(), "rb");
-    if (!fp) return doc;
-    char readBuffer[65536];
-    rapidjson::FileReadStream is(fp, readBuffer, sizeof(readBuffer));
-    doc.ParseStream(is);
-    fclose(fp);
+    doc.Parse(jsonStr.c_str());
+
+    if (doc.HasParseError()) {
+        throw std::runtime_error(
+            std::string("JSON parse error: ") +
+            rapidjson::GetParseError_En(doc.GetParseError()) +
+            " (offset " + std::to_string(doc.GetErrorOffset()) + ")"
+        );
+    }
+
     return doc;
 }
+
 
 
