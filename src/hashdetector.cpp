ed5866d733d10bb18efe304fcd75b7b49ecfb25c
Richard Callaby <rcallaby@gmail.com>
2025-08-20 18:38:43 -0500

    updated the release workflow (#44)

--- src/hashdetector.cpp
+++ src/hashdetector.cpp
@@ -4,13 +4,28 @@ HashDetector::HashDetector() {
     // Initialize hash types with characteristics and hashcat modes
     hashTypes = {
         {"Adler32", 8, std::regex("^[0-9A-Fa-f]{8}$"), "", 14100},
-        {"Argon2", 96, std::regex("^\\$argon2[di]?[A-Za-z0-9$=,]+$"), "$argon2", 13300},
+        {"Argon2", 96, std::regex("^\\$argon2(id|i|d)?\\$[A-Za-z0-9$=,]+$"), "$argon2", 13300},
+        {"BLAKE2b-512", 128, std::regex("^[0-9a-fA-F]{128}$"), "", 600},
+        {"BLAKE2s-256", 64, std::regex("^[0-9a-fA-F]{64}$"), "", 600},
         {"bcrypt", 60, std::regex("^\\$2[aby]\\$[0-9]{2}\\$[./A-Za-z0-9]{53}$"), "$2", 3200},
+        {"CHAP", 32, std::regex("^[0-9a-fA-F]{32}$"), "", 5003},
         {"CRC32", 8, std::regex("^[0-9A-Fa-f]{8}$"), "", 11500},
+        {"DES crypt", 13, std::regex("^[./0-9A-Za-z]{13}$"), "", 1500},
+        {"GOST R 34.11-94", 64, std::regex("^[0-9a-fA-F]{64}$"), "", 6900},
+        {"GOST R 34.11-2012 (Streebog-256)", 64, std::regex("^[0-9a-fA-F]{64}$"), "", 11700},
+        {"GOST R 34.11-2012 (Streebog-512)", 128, std::regex("^[0-9a-fA-F]{128}$"), "", 11800},
+        {"HAVAL-128", 32, std::regex("^[0-9a-fA-F]{32}$"), "", 8800},
+        {"HAVAL-160", 40, std::regex("^[0-9a-fA-F]{40}$"), "", 8800},
+        {"HAVAL-192", 48, std::regex("^[0-9a-fA-F]{48}$"), "", 8800},
+        {"HAVAL-224", 56, std::regex("^[0-9a-fA-F]{56}$"), "", 8800},
+        {"HAVAL-256", 64, std::regex("^[0-9a-fA-F]{64}$"), "", 8800},
+        {"Kerberos 5 etype 23", 32, std::regex("^[0-9A-Fa-f]{32}$"), "", 7500},
+        {"Kerberos 5 etype 18 (AES256-CTS-HMAC-SHA1-96)", 64, std::regex("^[0-9A-Fa-f]{64}$"), "", 19900},
         {"LM", 32, std::regex("^[0-9A-Fa-f]{32}$"), "", 3000},
         {"MD2", 32, std::regex("^[0-9A-Fa-f]{32}$"), "", 800},
         {"MD4", 32, std::regex("^[0-9a-fA-F]{32}$"), "", 900},
         {"MD5", 32, std::regex("^[0-9a-fA-F]{32}$"), "", 0},
+        {"MD5 crypt", 34, std::regex("^\\$1\\$[./A-Za-z0-9]{1,8}\\$[./A-Za-z0-9]{22}$"), "$1$", 500},
         {"MSCACHEv1", 32, std::regex("^[0-9A-Fa-f]{32}$"), "", 1100},
         {"MSCACHEv2", 32, std::regex("^[0-9A-Fa-f]{32}$"), "", 2100},
         {"NetNTLMv1", 48, std::regex("^[0-9A-Fa-f]{48}$"), "", 5500},
@@ -19,6 +34,7 @@ HashDetector::HashDetector() {
         {"PBKDF2-HMAC-SHA1", 160, std::regex("^\\$pbkdf2-sha1\\$[0-9]+\\$[A-Za-z0-9+/=]+\\$[A-Za-z0-9+/=]+$"), "$pbkdf2", 12001},
         {"PBKDF2-HMAC-SHA256", 256, std::regex("^\\$pbkdf2-sha256\\$[0-9]+\\$[A-Za-z0-9+/=]+\\$[A-Za-z0-9+/=]+$"), "$pbkdf2", 10900},
         {"PBKDF2-HMAC-SHA512", 512, std::regex("^\\$pbkdf2-sha512\\$[0-9]+\\$[A-Za-z0-9+/=]+\\$[A-Za-z0-9+/=]+$"), "$pbkdf2", 12100},
+        {"RIPEMD-128", 32, std::regex("^[0-9A-Fa-f]{32}$"), "", 6000},
         {"RIPEMD-160", 40, std::regex("^[0-9A-Fa-f]{40}$"), "", 6000},
         {"scrypt", 64, std::regex("^\\$scrypt\\$[0-9]+\\$[A-Za-z0-9+/=]+\\$[A-Za-z0-9+/=]+$"), "$scrypt", 8900},
         {"SHA1", 40, std::regex("^[0-9a-fA-F]{40}$"), "", 100},
@@ -30,10 +46,17 @@ HashDetector::HashDetector() {
         {"SHA3-256", 64, std::regex("^[0-9a-fA-F]{64}$"), "", 17600},
         {"SHA3-384", 96, std::regex("^[0-9a-fA-F]{96}$"), "", 17400},
         {"SHA3-512", 128, std::regex("^[0-9a-fA-F]{128}$"), "", 17800},
+        {"Snefru-128", 32, std::regex("^[0-9a-fA-F]{32}$"), "", 13000},
+        {"Snefru-256", 64, std::regex("^[0-9a-fA-F]{64}$"), "", 13000},
+        {"Tiger-128", 32, std::regex("^[0-9a-fA-F]{32}$"), "", 10000},
+        {"Tiger-160", 40, std::regex("^[0-9a-fA-F]{40}$"), "", 10000},
+        {"Tiger-192", 48, std::regex("^[0-9a-fA-F]{48}$"), "", 10000},
+        {"Whirlpool", 128, std::regex("^[0-9a-fA-F]{128}$"), "", 6100},
         {"WPA-PMK", 64, std::regex("^[0-9a-fA-F]{64}$"), "", 2500},
-        {"WPA-PMKID", 32, std::regex("^[0-9a-fA-F]{32}$"), "", 16800},
+        {"WPA-PMKID", 32, std::regex("^[0-9a-fA-F]{32}$"), "", 16800}
     };
 }
+
 
 
 std::string HashDetector::detectHashType(const std::string& hash, int &hashcatMode) {
